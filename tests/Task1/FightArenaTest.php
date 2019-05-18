86b27f4c1ff867b30afb69cf7f5ad91bb9d579c1
Mariia128 <mariia.tarantsova@ipayless.co.il>
2019-05-18 20:07:53 +0300

    initial commit

---
+++ tests/Task1/FightArenaTest.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Tests\Task1;
+
+use PHPUnit\Framework\TestCase;
+use App\Task1\FightArena;
+use App\Task1\Fighter;
+
+class FightArenaTest extends TestCase
+{
+    /**
+     * @var FightArena
+     */
+    private $arena;
+
+    /**
+     * @var Fighter
+     */
+    private $fighter1;
+
+    /**
+     * @var Fighter
+     */
+    private $fighter2;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->arena = new FightArena();
+
+        $this->fighter1 = new Fighter(
+            1,
+            'Ryu',
+            100,
+            10,
+            'https://bit.ly/2E5Pouh'
+        );
+
+        $this->fighter2 = new Fighter(
+            2,
+            'Chun-Li',
+            70,
+            30,
+            'https://bit.ly/2Vie3lf'
+        );
+    }
+
+    public function testAll()
+    {
+        $this->arena->add($this->fighter1);
+        $this->arena->add($this->fighter2);
+
+        $this->assertCount(2, $this->arena->all());
+    }
+
+    public function testAdd()
+    {
+        $this->arena->add($this->fighter1);
+        $this->arena->add($this->fighter2);
+
+        $this->assertEquals($this->fighter1->getId(), $this->arena->all()[0]->getId());
+        $this->assertEquals($this->fighter2->getId(), $this->arena->all()[1]->getId());
+    }
+
+    public function testMostPowerful()
+    {
+        $this->arena->add($this->fighter1);
+        $this->arena->add($this->fighter2);
+
+        $this->assertEquals(30, $this->arena->mostPowerful()->getAttack());
+    }
+
+    public function testMostHealty()
+    {
+        $this->arena->add($this->fighter1);
+        $this->arena->add($this->fighter2);
+
+        $this->assertEquals(100, $this->arena->mostHealthy()->getHealth());
+    }
+}
---
+++ tests/Task3/BuiltInServerTest.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Tests\Task3;
+
+use PHPUnit\Framework\TestCase;
+
+class BuiltInServerTest extends TestCase
+{
+    const TEST_REGEX = '/<img src=\"https:\/\/bit\.ly\/(2E5Pouh|2Vie3lf|2VZ2tQd)\">/';
+
+    /**
+     * @var BuiltInServerRunner
+     */
+    private $runner;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->runner = new BuiltInServerRunner();
+        $this->runner->start();
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+
+        $this->runner->stop();
+    }
+
+    public function testBuiltInServerRuns()
+    {
+        $page = file_get_contents(BuiltInServerRunner::TEST_ENDPOINT);
+
+        $this->assertStringContainsString('<title>Built-in Web Server</title>', $page);
+    }
+
+    public function testShowsAll()
+    {
+        $page = file_get_contents(BuiltInServerRunner::TEST_ENDPOINT);
+
+        $this->assertRegExp(self::TEST_REGEX, $page);
+    }
+
+    public function testFightersAmount()
+    {
+        $page = file_get_contents(BuiltInServerRunner::TEST_ENDPOINT);
+
+        $matches = [];
+
+        preg_match_all(self::TEST_REGEX, $page, $matches, PREG_SET_ORDER, 0);
+
+        $this->assertCount(3, $matches);
+    }
+
+    public function testFightersInfoRendered()
+    {
+        $page = file_get_contents(BuiltInServerRunner::TEST_ENDPOINT);
+
+        $matches = [];
+
+        preg_match_all('/([\w\s-]+)\: (\d+), (\d+)/', $page, $matches, PREG_SET_ORDER, 0);
+
+        $names = array_map(
+            function ($match) {
+                return $match[1];
+            },
+            $matches
+        );
+
+        $expected = ['Ryu', 'Chun-Li', 'Ken Masters'];
+
+        $this->assertCount(3, $names);
+        $this->assertCount(0, array_diff($expected, $names));
+    }
+
+    public function testFighterImageRendered()
+    {
+        $page = file_get_contents(BuiltInServerRunner::TEST_ENDPOINT);
+
+        $matches = [];
+
+        preg_match_all(self::TEST_REGEX, $page, $matches, PREG_SET_ORDER, 0);
+
+        $imageIds = array_map(
+            function ($match) {
+                return $match[1];
+            },
+            $matches
+        );
+
+        $expected = ['2E5Pouh', '2Vie3lf', '2VZ2tQd'];
+
+        $this->assertCount(3, $imageIds);
+        $this->assertCount(0, array_diff($expected, $imageIds));
+    }
+}
